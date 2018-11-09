366ebbb2377bb43e225023f6013f03ebbd9b4f27
Adam <belkinmuj@gmail.com>
2018-11-09 18:48:36 +0300

    types

--- python/Blender.py
+++ python/Blender.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def src_dest_weighted_average(img1, img2,
-      measure: "probability measure" = 0.5) -> "Weighted average of 2 images":
+      measure: float = 0.5) -> np.array:
     """Measure is assummed to be in [0,1]"""
 
     return np.asarray(img1 * measure + img2 * (1 - measure), dtype = np.uint8)
--- python/Locator.py
+++ python/Locator.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 def src_dest_average_points(start_points,
-    end_points, measure: float = 0.5) -> float:
+    end_points, measure: float = 0.5) -> np.array:
   """ Generated using measure param.
       Measure is assumed to be in [0,1]
   """
